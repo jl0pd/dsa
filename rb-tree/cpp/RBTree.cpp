048ba59703bd17aaeca523e9ee227472f96b83a8
JL0PD <jl0pd@outlook.com>
2018-09-27 21:21:54 +0700

    1

--- rb-tree/cpp/RBTree.cpp
+++ rb-tree/cpp/RBTree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <exception>
 
 class RBTree
 {
@@ -54,26 +55,24 @@ private:
             #define RED "\x1b[41m"
             #define BLACK "\x1b[40m"
             #define DEFAULT "\x1b[49m"
-            #define DIVIDER "\n~~~~~~~~\n"
 
             if (this->color)
             {
                 std::cout << BLACK <<
                 "key: " << this->key <<
                 "\nvalue: " << this->value <<
-                DEFAULT << DIVIDER;
+                DEFAULT << std::endl;
             }
             else
             {
                 std::cout << RED <<
                 "key: " << this->key <<
                 "\nvalue: " << this->value <<
-                DEFAULT << DIVIDER;
+                DEFAULT << std::endl;
             }
             #undef RED
             #undef BLACK
             #undef DEFAULT
-            #undef DIVIDER
         }
     };
 
@@ -81,9 +80,8 @@ private:
     Node *nullNode;
     void add_fixup(Node *node)
     {
+        // this->print();
         Node *uncle;
-        std::cout << "~~~~~~~~~fixupping" << std::endl;
-
         while (node != this->root && node->parent->color == this->color::red)
         {
             if (node->parent == node->parent->parent->left)
@@ -112,12 +110,8 @@ private:
             else
             {
                 uncle = node->parent->parent->left;
-                uncle->print();
-                std::cout << "after uncle" << std::endl;
                 if (uncle->color == this->color::red)
                 {
-                    std::cout << "in if" << std::endl;
-                
                     node->parent->parent->color = this->color::red;
                     node->parent->color = this->color::black;
                     uncle->color = this->color::black;
@@ -125,7 +119,6 @@ private:
                 }
                 else
                 {
-                    std::cout << "in else" << std::endl;
                     if (node == node->parent->left)
                     {
                         node = node->parent;
@@ -139,9 +132,7 @@ private:
         }
 
         this->root->color = this->color::black;
-
-        std::cout << "~~~~~~~~~fixupped" << std::endl;
-
+        // this->print();
     }
 
     void left_rotate(Node *node)
@@ -157,24 +148,22 @@ private:
         if (right != this->nullNode)
         {
             right->parent = node->parent;
-            if (node->parent != this->nullNode)
+        }
+
+        if (node->parent != this->nullNode)
+        {
+            if (node == node->parent->left)
             {
-                if (node->parent != this->nullNode)
-                {
-                    if (node == node->parent->left)
-                    {
-                        node->parent->left = right;
-                    }
-                    else
-                    {
-                        node->parent->right = right;
-                    }
-                }
-                else
-                {
-                    this->root = right;
-                }
+                node->parent->left = right;
             }
+            else
+            {
+                node->parent->right = right;
+            }
+        }
+        else
+        {
+            this->root = right;
         }
 
         right->left = node;
@@ -197,26 +186,24 @@ private:
         if (left != this->nullNode)
         {
             left->parent = node->parent;
-            if (node->parent != this->nullNode)
-            {
-                if (node->parent != this->nullNode)
-                {
-                    if (node == node->parent->right)
-                    {
-                        node->parent->right = left;
-                    }
-                    else
-                    {
-                        node->parent->left = left;
-                    }
-                }
-                else
-                {
-                    this->root = left;
-                }
-            }
         }
 
+        if (node->parent != this->nullNode)
+        {
+            if (node == node->parent->right)
+            {
+                node->parent->right = left;
+            }
+            else
+            {
+                node->parent->left = left;
+            }
+        }
+        else
+        {
+            this->root = left;
+        }
+    
         left->right = node;
         if (node != this->nullNode)
         {
@@ -224,23 +211,27 @@ private:
         }
     }
     
-    void print(Node *root)
+    void print(Node *node)
     {
-        if(root->value[0])
+        if(node != this->nullNode)
         {
-            root->print();
-            this->print(root->left);
-            this->print(root->right);
+            node->print();
+            this->print(node->left);
+            this->print(node->right);
+        }
+        else
+        {
+            std::cout << "\n~~~~~~~~~\n";
         }
     }
 
-    void clear(Node *root)
+    void clear(Node *node)
     {
-        if (this->root->value[0])
+        if (node != this->nullNode)
         {
-            this->clear(this->root->left);
-            this->clear(this->root->right);
-            delete this->root;
+            this->clear(node->left);
+            this->clear(node->right);
+            delete node;
         }
     }
 
@@ -266,12 +257,11 @@ private:
 public:
     RBTree()
     {
-        this->root = new Node();
         this->nullNode = new Node(
             0, this->color::black, std::string(""), NULL, NULL, NULL
         );
 
-        // this->nullNode->parent = this->nullNode->left = this->nullNode->right = this->nullNode;
+        this->root = this->nullNode;
     }
 
     RBTree(std::vector<int> array) : RBTree()
@@ -308,7 +298,7 @@ public:
 
     void clear()
     {
-        if (this->root->value[0])
+        if (this->root != this->nullNode)
         {
             this->clear(this->root->left);
             this->clear(this->root->right);
@@ -320,6 +310,7 @@ public:
 
     void add(int key, std::string value)
     {
+        std::cout << "key: " << key << " value: " << value << std::endl;
         Node *node = this->root;
         Node *parent = this->nullNode;
 
@@ -362,19 +353,21 @@ public:
         }
 
         this->add_fixup(node);
-        std::cout << "~~~~~~~~~added key: " << key << " value: " << value << "\n\n\n\n";
-
     }
 
     void print()
     {
-        if(this->root->value[0])
+        if(this->root != this->nullNode)
         {
             this->root->print();
             this->print(this->root->left);
             this->print(this->root->right);
         }
-    }
+        else
+        {
+            std::cout << "~~~~~~~~~\n";
+        }
+    }   
 
     std::string min()
     {
@@ -396,4 +389,187 @@ public:
         return tmp->value;
     }
 
+
+    void del(int key)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+
+        Node *p = this->root;
+
+        Node *y = NULL;
+        Node *node = NULL;
+
+        int found = 0;
+
+        while(p != NULL && found == 0)
+        {
+            if(p->key == key)
+            {
+                found = 1;
+            }
+
+            if(found == 0)
+            {
+                if(p->key < key)
+                    p = p->right;
+                else
+                    p = p->left;
+            }
+        }
+
+        if(found == 0)
+        {
+            throw std::exception();
+        }
+        else
+        {
+            if(p->left == NULL || p->right == NULL)
+            {
+                y = p;
+            }
+            else
+            {
+                y = successor(p);
+            }
+
+            if(y->left!=NULL)
+            {           
+                node=y->left;
+            }
+            else
+            {
+                if(y->right!=NULL)
+                { 
+                    node=y->right;
+                }
+                else
+                {
+                    node=NULL;
+                }
+            }
+            
+            if(node!=NULL)
+            {
+                node->parent=y->parent;
+            }
+
+            if(y->parent==NULL)
+            {
+                root=node;
+            }
+            else
+            {
+                if(y==y->parent->left)
+                {
+                    y->parent->left=node;
+                }
+                else
+                {
+                    y->parent->right=node;
+                }
+            }
+
+            if(y!=p)
+            {
+                p->color=y->color;
+                p->key=y->key;
+            }
+
+            if(y->color=='b')
+                delfix(node);
+        }
+    }
+
+    void delfix(Node *p)
+    {
+        node *s;
+        while(p!=root&&p->color=='b')
+        {
+            if(p->parent->left==p)
+            {
+                    s=p->parent->right;
+                    if(s->color=='r')
+                    {
+                            s->color='b';
+                            p->parent->color='r';
+                            leftrotate(p->parent);
+                            s=p->parent->right;
+                    }
+                    if(s->right->color=='b'&&s->left->color=='b')
+                    {
+                            s->color='r';
+                            p=p->parent;
+                    }
+                    else
+                    {
+                        if(s->right->color=='b')
+                        {
+                                s->left->color=='b';
+                                s->color='r';
+                                rightrotate(s);
+                                s=p->parent->right;
+                        }
+                        s->color=p->parent->color;
+                        p->parent->color='b';
+                        s->right->color='b';
+                        leftrotate(p->parent);
+                        p=root;
+                    }
+            }
+            else
+            {
+                    s=p->parent->left;
+                    if(s->color=='r')
+                    {
+                            s->color='b';
+                            p->parent->color='r';
+                            rightrotate(p->parent);
+                            s=p->parent->left;
+                    }
+                    if(s->left->color=='b'&&s->right->color=='b')
+                    {
+                            s->color='r';
+                            p=p->parent;
+                    }
+                    else
+                    {
+                            if(s->left->color=='b')
+                            {
+                                s->right->color='b';
+                                s->color='r';
+                                leftrotate(s);
+                                s=p->parent->left;
+                            }
+                            s->color=p->parent->color;
+                            p->parent->color='b';
+                            s->left->color='b';
+                            rightrotate(p->parent);
+                            p=root;
+                    }
+            }
+        p->color='b';
+        root->color='b';
+        }
+    }
+
+node* RBtree::successor(node *p)
+{
+      node *y=NULL;
+     if(p->left!=NULL)
+     {
+         y=p->left;
+         while(y->right!=NULL)
+              y=y->right;
+     }
+     else
+     {
+         y=p->right;
+         while(y->left!=NULL)
+              y=y->left;
+     }
+     return y;
+}
 };
--- rb-tree/cpp/main-c.cpp
+++ rb-tree/cpp/main-c.cpp
@@ -12,12 +12,16 @@ int main(int argc, char **argv)
 
     std::vector<int> arr;
     for(int i = 1; i < 10; i++){
-        arr.push_back(i);
+        arr.push_back(rand() % 100);
     }
+
+    // std::vector<int> arr = {11, 2, 7, 5, 8, 1, 14, 15};
 
     RBTree *my_tree = new RBTree(arr);
 
     my_tree->print();
+
+    std::cout << my_tree->min() << " <-min || max-> " << my_tree->max() << std::endl;
 
     delete my_tree;
 
